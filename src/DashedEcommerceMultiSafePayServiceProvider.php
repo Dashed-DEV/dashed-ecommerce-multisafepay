108e19d659848ab2173d6e826ceb70d32977166f
Robinvm <22294849+Robinvm@users.noreply.github.com>
2025-01-29 07:59:51 +0000

    Styling fixed with PHP CS Fixer

--- src/DashedEcommerceMultiSafePayServiceProvider.php
+++ src/DashedEcommerceMultiSafePayServiceProvider.php
@@ -2,7 +2,6 @@
 
 namespace Dashed\DashedEcommerceMultiSafePay;
 
-use Dashed\DashedCore\DashedCorePlugin;
 use Spatie\LaravelPackageTools\Package;
 use Illuminate\Console\Scheduling\Schedule;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
